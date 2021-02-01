8c40e3a4e84959f1ef65fd13ba031df245f0c525
MrPig91 <sirius.cleveland@gmail.com>
2021-01-31 22:55:03 -0600

    Renamed Path paramter for import-xaml

--- PS_PerformanceMonitor/Private/Import-Xaml.ps1
+++ PS_PerformanceMonitor/Private/Import-Xaml.ps1
@@ -1,9 +1,9 @@
 function Import-Xaml {
     param(
-        $PathToXAML
+        $Path
     )
     Add-Type -AssemblyName PresentationFramework
-    [xml]$xaml = Get-Content -Path $PathToXAML
+    [xml]$xaml = Get-Content -Path $Path
     $manager = [System.Xml.XmlNamespaceManager]::new($xaml.NameTable)
     $manager.AddNamespace("x","http://schemas.microsoft.com/winfx/2006/xaml")
     $xamlReader = [System.Xml.XmlNodeReader]::new($xaml)
