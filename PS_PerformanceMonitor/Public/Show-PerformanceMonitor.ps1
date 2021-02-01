f92c5e177f5a3cfe0c31f30a3a435db70c3f68f8
MrPig91 <sirius.cleveland@gmail.com>
2021-01-31 23:41:01 -0600

    Added computer data to data hashtable

--- PS_PerformanceMonitor/Public/Show-PerformanceMonitor.ps1
+++ PS_PerformanceMonitor/Public/Show-PerformanceMonitor.ps1
@@ -13,10 +13,13 @@ function Show-PerformanceMonitor {
     $RunspacePool.ThreadOptions = "ReuseThread"
     $RunspacePool.open()
 
+    #DataHash Adding Properties
     $DataHash.ModuleRoot = $MyInvocation.MyCommand.Module.ModuleBase
     $DataHash.PrivateFunctions = Join-Path -Path $DataHash.ModuleRoot -ChildPath "Private"
     $DataHash.Assemblies = Join-Path -Path $DataHash.ModuleRoot -ChildPath "Assemblies"
     $DataHash.WPF = Join-Path -Path $DataHash.ModuleRoot -ChildPath "WPF"
+    $DataHash.AllComputers = New-Object System.Collections.Generic.List[System.Object]
+    $DataHash.addedComputers = New-Object System.Collections.Generic.List[System.Object]
 
     #Import required assemblies and private functions
     Get-childItem -Path $DataHash.PrivateFunctions -File | ForEach-Object {Import-Module $_.FullName}
