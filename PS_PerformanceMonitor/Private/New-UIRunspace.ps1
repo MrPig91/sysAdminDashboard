13a2f96200431d27520d27dadc89168e96617c72
MrPig91 <sirius.cleveland@gmail.com>
2021-01-31 23:30:12 -0600

    Added try catch block for starting UI

--- PS_PerformanceMonitor/Private/New-UIRunspace.ps1
+++ PS_PerformanceMonitor/Private/New-UIRunspace.ps1
@@ -1,19 +1,27 @@
 function New-UIRunspace{
     [powershell]::Create().AddScript{
+        $ErrorActionPreference = "Stop"
         Add-Type -AssemblyName PresentationFramework
-        Add-Type -AssemblyName WindowsFormsIntegration
-
         #Import required assemblies and private functions
-        Get-childItem -Path $DataHash.PrivateFunctions | ForEach-Object {Import-Module $_.FullName}
-        Get-childItem -Path $DataHash.Assemblies | ForEach-Object {Add-Type -AssemblyName $_.FullName}
+        Get-childItem -Path $DataHash.PrivateFunctions -File | ForEach-Object {Import-Module $_.FullName}
+        Get-childItem -Path $DataHash.Assemblies -File | ForEach-Object {Add-Type -Path $_.FullName}
 
-        $MainWindow = Import-Xaml -Path $DataHash.WPF\MainWindow.xaml
-        $UIHash.MainWindow = $MainWindow
-        $UIHash.MainTablControl = $MainWindow.FindName("MainTabControl")
-        $UIHash.CPUTabPage = $MainWindow.FindName("CPUTabPage")
-        $UIHash.AddComputerButton = $MainWindow.FindName("AddComputerButton")
-        $UIHash.SelectAllButton = $MainWindow.FindName("SelectAllButton")
-        $UIHash.DeSelectAllButton = $MainWindow.FindName("DeSelectAllButton")
-        $UIHash.RemoveSelectedButton = $MainWindow.FindName("RemoveSelectedButton")
+        Try{
+            $XAMLPath = Join-Path -Path $DataHash.WPF -ChildPath MainWindow.xaml
+            $MainWindow = Import-Xaml -Path $XAMLPath
+            $UIHash.MainWindow = $MainWindow
+            $UIHash.MainTablControl = $MainWindow.FindName("MainTabControl")
+            $UIHash.CPUTabPage = $MainWindow.FindName("CPUTabPage")
+            $UIHash.AddComputerButton = $MainWindow.FindName("AddComputerButton")
+            $UIHash.SelectAllButton = $MainWindow.FindName("SelectAllButton")
+            $UIHash.DeSelectAllButton = $MainWindow.FindName("DeSelectAllButton")
+            $UIHash.RemoveSelectedButton = $MainWindow.FindName("RemoveSelectedButton")
+    
+            $UIHash.MainWindow.ShowDialog()
+        }
+        catch{
+            [System.Windows.MessageBox]::Show($_.Exception.Message)
+            #Show-Messagebox -Text $_.Exception.Message
+        }
     }
 }
