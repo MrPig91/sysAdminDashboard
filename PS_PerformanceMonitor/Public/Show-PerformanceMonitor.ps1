59bd874003c4f9766c2a3ea48d4741d5a18fe8b7
MrPig91 <sirius.cleveland@gmail.com>
2021-02-01 21:24:28 -0600

    Added temp global vars for hashes for debuging and script hash

--- PS_PerformanceMonitor/Public/Show-PerformanceMonitor.ps1
+++ PS_PerformanceMonitor/Public/Show-PerformanceMonitor.ps1
@@ -1,13 +1,16 @@
 function Show-PerformanceMonitor {
     Add-Type -AssemblyName PresentationFramework
 
-    $UIHash = [hashtable]::Synchronized(@{})
-    $DataHash = [hashtable]::Synchronized(@{})
+    $Global:UIHash = [hashtable]::Synchronized(@{})
+    $Global:DataHash = [hashtable]::Synchronized(@{})
+    $ScriptsHash = [hashtable]::Synchronized(@{})
     $InitialSessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
     $UISync = [System.Management.Automation.Runspaces.SessionStateVariableEntry]::new("UIHash", $UIHash, $Null)
     $DataSync = [System.Management.Automation.Runspaces.SessionStateVariableEntry]::new("DataHash", $DataHash, $Null)
+    $ScriptsSync = [System.Management.Automation.Runspaces.SessionStateVariableEntry]::new("ScriptsHash", $ScriptsHash, $Null)
     $InitialSessionState.Variables.Add($UISync)
     $InitialSessionState.Variables.Add($DataSync)
+    $InitialSessionState.Variables.Add($ScriptsSync)
     $RunspacePool = [runspacefactory]::CreateRunspacePool(1,5,$InitialSessionState,$Host)
     $RunspacePool.ApartmentState = "STA"
     $RunspacePool.ThreadOptions = "ReuseThread"
@@ -18,8 +21,7 @@ function Show-PerformanceMonitor {
     $DataHash.PrivateFunctions = Join-Path -Path $DataHash.ModuleRoot -ChildPath "Private"
     $DataHash.Assemblies = Join-Path -Path $DataHash.ModuleRoot -ChildPath "Assemblies"
     $DataHash.WPF = Join-Path -Path $DataHash.ModuleRoot -ChildPath "WPF"
-    $DataHash.AllComputers = New-Object System.Collections.Generic.List[System.Object]
-    $DataHash.addedComputers = New-Object System.Collections.Generic.List[System.Object]
+    $DataHash.Classes = Join-Path -Path $DataHash.ModuleRoot -ChildPath "Classes"
 
     #Import required assemblies and private functions
     Get-childItem -Path $DataHash.PrivateFunctions -File | ForEach-Object {Import-Module $_.FullName}
@@ -29,4 +31,14 @@ function Show-PerformanceMonitor {
     $UIRunspace = NEw-UIRunspace
     $UIRunspace.RunspacePool = $RunspacePool
     [void]$UIRunspace.BeginInvoke()
+
+    #Create grabbing computers from AD Thread
+    $GetComputersRunspace = New-GetComputerRunspace
+    $GetComputersRunspace.RunspacePool = $RunspacePool
+    [void]$GetComputersRunspace.BeginInvoke()
+
+    #Add Ping Script To RunspacePool
+    $PingRunspace = New-PingRunspace
+    $PingRunspace.RunspacePool = $RunspacePool
+    $ScriptsHash.Ping = $PingRunspace
 }
