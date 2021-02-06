a921a7e15df98271c0932e3d5ad38186b5409216
MrPig91 <sirius.cleveland@gmail.com>
2021-02-06 09:50:32 -0600

    Updated class to add diskinfo and loggedinuser

--- PS_PerformanceMonitor/Classes/ComputerListViewItem.ps1
+++ PS_PerformanceMonitor/Classes/ComputerListViewItem.ps1
@@ -1,13 +1,72 @@
 Add-Type -Language CSharp @'
 using System.ComponentModel;
+using LiveCharts;
+using System.Collections.ObjectModel;
+
+public class DiskInfo
+{
+    public double TotalSize { get; set; }
+    public double FreeSpace { get; set; }
+    public double PercentFree {get; set; }
+    public bool HasData {get; set; }
+    public double UsedSpace { get; set; }
+
+    public LiveCharts.ChartValues<double> UsedSpaceValues { get; set; }
+    public LiveCharts.ChartValues<double> FreeSpaceValues { get; set; }
+
+    public DiskInfo (double totalsize, double freespace)
+    {
+        if (totalsize != 0)
+        {
+            PercentFree = System.Math.Round((freespace / totalsize) * 100,2);
+        }
+        else
+        {
+            PercentFree = 0;
+        }
+
+        TotalSize = totalsize;
+        FreeSpace = freespace;
+        UsedSpace = totalsize - freespace;
+
+        UsedSpaceValues = new LiveCharts.ChartValues<double>();
+        FreeSpaceValues = new LiveCharts.ChartValues<double>();
+
+        UsedSpaceValues.Add(totalsize - freespace);
+        FreeSpaceValues.Add(freespace);
+    }
+}
+
 public class ComputerListViewItem : INotifyPropertyChanged
     {
         private bool _ischecked;
+        private DiskInfo _diskinfo;
+        private ObservableCollection<System.Object> _loggedinuser;
+
         public string ComputerName { get; set; }
         public string IPAddress { get; set; }
         public string SerialNumber { get; set; }
         public string OperatingSystem { get; set; }
         public bool Online { get; set; }
+        public ObservableCollection<System.Object> LoggedInUser
+        {
+            get {return _loggedinuser;}
+            set{
+                _loggedinuser = value;
+                NotifyPropertyChanged("LoggedInUser");
+            }
+        }
+
+        public DiskInfo diskInfo
+        {
+            get {return _diskinfo; }
+            set
+            {
+                _diskinfo = value;
+                NotifyPropertyChanged("diskInfo");
+            }
+        }
+        public bool Scanned { get; set; }
         public bool IsChecked
         {
             get {return _ischecked; }
@@ -35,6 +94,8 @@ public class ComputerListViewItem : INotifyPropertyChanged
             IPAddress = IP;
             SerialNumber = sn;
             IsChecked = ischecked;
+            diskInfo = new DiskInfo(0, 0);
+            LoggedInUser = new ObservableCollection<System.Object>();
         }
     }
-'@
+'@ -ReferencedAssemblies (Get-childItem -Path $DataHash.Assemblies -File | ForEach-Object {$_.FullName})
