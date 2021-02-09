90bf7b1715e6ff8995b163b1a14c323fd1dba4eb
MrPig91 <sirius.cleveland@gmail.com>
2021-02-08 21:03:41 -0600

    Removed old comments

--- PS_PerformanceMonitor/Private/New-PingRunspace.ps1
+++ PS_PerformanceMonitor/Private/New-PingRunspace.ps1
@@ -11,49 +11,6 @@ function New-PingRunspace {
                     $computer.Scanned = $true
                     Start-ComputerScan -Computer $computer
                 }
-
-                #Add All Events for Item Control UI Elements
-               <#  $UIHash.MainWindow.Dispatcher.Invoke([action]{
-                     $presenter = $UIHash.ComputerOverview.ItemContainerGenerator.ContainerFromIndex(0)
-
-                    $UIHash.ADButton = $UIHash.ComputerOverview.ItemContainerGenerator.ContainerFromIndex(0).ContentTemplate.FindName("ADButton",$presenter)
-                    $UIHash.LoggedInUsers = $UIHash.ComputerOverview.ItemContainerGenerator.ContainerFromIndex(0).ContentTemplate.FindName("LoggedInUsers",$presenter)
-                    $UIHash.LogOffUserButton = $UIHash.ComputerOverview.ItemContainerGenerator.ContainerFromIndex(0).ContentTemplate.FindName("LogOffUserButton",$presenter)
-
-                    $UIHash.ADButton.Add_MouseDoubleClick({
-                        Show-Object $this
-                    })
-
-                    $UIHash.LoggedInUsers.ADD_SelectionChanged({
-                        if ($_.AddedItems){
-                            $DataHash.LoggedInUsersSelectedItem = $_.AddedItems
-                            #$UIHash.LogOffUserButton.Dispatcher.Invoke([action]{$UIHash.LogOffUserButton.IsEnabled = $true})
-                        }
-                        else{
-                            $UIHash.LogOffUserButton.IsEnabled = $false
-                        }
-                    })
-
-                    $UIHash.LoggedInUsers.ADD_MouseDoubleClick({
-                        try{
-                            # launch user window
-                        }
-                        catch{
-                            Show-Messagebox -Text $_.Exception.Message -Title "Double Click Logged In User Box Error" -Icon Error
-                        }
-                    })
-
-                    $UIHash.LogOffUserButton.Add_Click({
-                        try{
-                            $DataHash.LoggedInUsersSelectedItem.LogOffUser()
-                            $UIHash.LoggedInUser.Items.Remove($DataHash.LoggedInUsersSelectedItem)
-                        }
-                        catch{
-                            Show-Messagebox -Text $_.Exception.Message -Title "Log Off User" -Icon Information
-                        }
-                    })
-
-                }) #>
             }
             else{
                 $computer.OnOrOff = "Offline"
